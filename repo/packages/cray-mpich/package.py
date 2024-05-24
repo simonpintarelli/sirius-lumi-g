093be8c942fc5d2651e2b540bc1388d6a1a6136e
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-05-24 18:16:39 +0200

    update

---
+++ repo/packages/cray-mpich/package.py
@@ -0,0 +1,181 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+import spack.compilers
+from spack.package import *
+
+
+class CrayMpich(Package):
+    """Install cray-mpich as a binary package"""
+
+    """Intended to override the main cray-mpich"""
+
+    homepage = "https://www.hpe.com/us/en/compute/hpc/hpc-software.html"
+    url = "file:///users/<USERNAME>/cray-mpich-binary-8.1.27.tar.gz"
+    maintainers = ["haampie"]
+
+    version(
+        "8.1.27",
+        sha256="0f4798eade03c4dd9d72c37598b2708618321d7a5e59460ea933f3531a33a92d"
+    )
+
+    variant("cuda", default=False)
+    variant("rocm", default=False)
+
+    conflicts("+cuda", when="+rocm", msg="Pick either CUDA or ROCM")
+
+    provides("mpi")
+
+    # Fix up binaries with patchelf.
+    depends_on("patchelf", type="build")
+    with when("+cuda"):
+        # libcudart.so.11.0
+        depends_on("cuda@11.0:11", type="link")
+
+    with when("+rocm"):
+        # libamdhip64.so.5
+        depends_on("hip@5:", type="link")
+        # libhsa-runtime64.so.1
+        depends_on("hsa-rocr-dev", type="link")
+
+    # libfabric.so.1
+    depends_on("libfabric@1:", type="link")
+
+    with when("%gcc"):
+        # libgfortran.so.5
+        conflicts("%gcc@:7")
+        for __compiler in spack.compilers.supported_compilers():
+            if __compiler != "gcc":
+                conflicts("%{}".format(__compiler), msg="gcc required")
+
+    with when("%nvhpc"):
+        conflicts("%nvhpc@:20.7")
+        for __compiler in spack.compilers.supported_compilers():
+            if __compiler != "nvhpc":
+                conflicts("%{}".format(__compiler), msg="nvhpc required")
+
+    def setup_run_environment(self, env):
+        env.set("MPICC", join_path(self.prefix.bin, "mpicc"))
+        env.set("MPICXX", join_path(self.prefix.bin, "mpic++"))
+        env.set("MPIF77", join_path(self.prefix.bin, "mpif77"))
+        env.set("MPIF90", join_path(self.prefix.bin, "mpif90"))
+
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        self.setup_run_environment(env)
+        env.set("MPICH_CC", dependent_spec.package.module.spack_cc)
+        env.set("MPICH_CXX", dependent_spec.package.module.spack_cxx)
+        env.set("MPICH_FC", dependent_spec.package.module.spack_fc)
+
+    def setup_dependent_package(self, module, dependent_spec):
+        self.spec.mpicc = join_path(self.prefix.bin, "mpicc")
+        self.spec.mpicxx = join_path(self.prefix.bin, "mpic++")
+        self.spec.mpifc = join_path(self.prefix.bin, "mpif90")
+        self.spec.mpif77 = join_path(self.prefix.bin, "mpif77")
+
+    def get_rpaths(self):
+        # Those rpaths are already set in the build environment, so
+        # let's just retrieve them.
+        pkgs = os.getenv("SPACK_RPATH_DIRS", "").split(":")
+        compilers = os.getenv("SPACK_COMPILER_IMPLICIT_RPATHS", "").split(":")
+        return ":".join([p for p in pkgs + compilers if p])
+
+    def should_patch(self, file):
+        # Returns true if non-symlink ELF file.
+        if os.path.islink(file):
+            return False
+        try:
+            with open(file, "rb") as f:
+                return f.read(4) == b"\x7fELF"
+        except OSError:
+            return False
+
+    def install(self, spec, prefix):
+        if "%nvhpc" in self.spec:
+            install_tree("mpich-nvhpc", prefix)
+        elif "%gcc" in self.spec:
+            install_tree("mpich-gcc", prefix)
+
+    @run_after("install")
+    def fixup_binaries(self):
+        patchelf = which("patchelf")
+        rpath = self.get_rpaths()
+        for root, _, files in os.walk(self.prefix):
+            for name in files:
+                f = os.path.join(root, name)
+                if not self.should_patch(f):
+                    continue
+                patchelf("--force-rpath", "--set-rpath", rpath, f, fail_on_error=False)
+                # The C compiler wrapper can fail because libmpi_gtl_cuda refers to the symbol
+                # __gxx_personality_v0 but wasn't linked against libstdc++.
+                if "libmpi_gtl_cuda.so" in str(f):
+                    patchelf("--add-needed", "libstdc++.so", f, fail_on_error=False)
+
+    @run_after("install")
+    def fixup_compiler_paths(self):
+        filter_file("@@CC@@", self.compiler.cc, self.prefix.bin.mpicc, string=True)
+        filter_file("@@CXX@@", self.compiler.cxx, self.prefix.bin.mpicxx, string=True)
+        filter_file("@@FC@@", self.compiler.fc, self.prefix.bin.mpifort, string=True)
+
+        filter_file("@@PREFIX@@", self.prefix, self.prefix.bin.mpicc, string=True)
+        filter_file("@@PREFIX@@", self.prefix, self.prefix.bin.mpicxx, string=True)
+        filter_file("@@PREFIX@@", self.prefix, self.prefix.bin.mpifort, string=True)
+
+        # link with the relevant gtl lib
+        if "+cuda" in self.spec:
+            gtl_library = "-lmpi_gtl_cuda"
+        elif "+rocm" in self.spec:
+            gtl_library = "-lmpi_gtl_hsa"
+        else:
+            gtl_library = ""
+
+        filter_file("@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpicc, string=True)
+        filter_file("@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpicxx, string=True)
+        filter_file(
+            "@@GTL_LIBRARY@@", gtl_library, self.prefix.bin.mpifort, string=True
+        )
+
+    @property
+    def headers(self):
+        hdrs = find_headers("mpi", self.prefix.include, recursive=True)
+        hdrs += find_headers(
+            "cray_version", self.prefix.include, recursive=True
+        )  # cray_version.h
+        # cray-mpich depends on cray-pmi
+        hdrs += find_headers(
+            "pmi", self.prefix.include, recursive=True
+        )  # See cray-pmi package
+        hdrs.directories = os.path.dirname(hdrs[0])
+        return hdrs
+
+    @property
+    def libs(self):
+        query_parameters = self.spec.last_query.extra_parameters
+
+        libraries = ["libmpi", "libmpich"]
+
+        if "f77" in query_parameters:
+            libraries.extend(["libmpifort", "libmpichfort", "libfmpi", "libfmpich"])
+
+        if "f90" in query_parameters:
+            libraries.extend(["libmpif90", "libmpichf90"])
+
+        if "+cuda" in self.spec:
+            libraries.append("libmpi*cuda")
+
+        if "+rocm" in self.spec:
+            libraries.append("libmpi*hsa")
+
+        libs = []
+        for lib_folder in [self.prefix.lib, self.prefix.lib64]:
+            libs += find_libraries(libraries, root=lib_folder, recursive=True)
+            # cray-mpich depends on cray-pmi
+            libs += find_libraries("libpmi", root=lib_folder, recursive=True)
+            libs += find_libraries("libopa", root=lib_folder, recursive=True)
+            libs += find_libraries("libmpl", root=lib_folder, recursive=True)
+            libs += find_libraries("libpals", root=lib_folder, recursive=True)
+
+        return libs
