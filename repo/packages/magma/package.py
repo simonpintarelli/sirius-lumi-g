2c1805fb1855a9b0b8bc10518ced37e5362761bc
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-09-19 17:41:42 +0200

    update magma recipe

--- repo/packages/magma/package.py
+++ repo/packages/magma/package.py
@@ -106,6 +106,8 @@ class Magma(CMakePackage, CudaPackage, ROCmPackage):
     patch("magma-2.5.0-cmake.patch", when="@2.5.0")
     patch("cmake-W.patch", when="@2.5.0:%nvhpc")
     patch("0001-fix-magma-build-error-with-rocm-6.0.0.patch", when="@2.7.2 ^hip@6.0 + rocm")
+    patch("0001-memoryType-type-in-rocm6.patch", when="@2.7.2 ^hip@6.0 + rocm")
+    patch("0001-hipsparse-include.patch", when="@2.7.2 ^hip@6.0 + rocm")
 
     @run_before("cmake")
     def generate_gpu_config(self):
