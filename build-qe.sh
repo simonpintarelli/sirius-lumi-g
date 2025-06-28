a87b8a953f71052d7e46dff3366fdf3d9422f135
Simon Pintarelli <1237199+simonpintarelli@users.noreply.github.com>
2025-06-29 00:40:19 +0200

    lumi-g June 2025 (#2)

---
+++ build-qe.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# TODO: change
+QESIRIUS_DIR=/scratch/project_465001618/sipintar/q-e-sirius
+
+# - make sirius available as dependency
+# - load cray-mpich (installed via spack)
+# - also provides dependencies for QE (HDF5, FFTW, openblas)
+# it was generated using
+# `spack -e ./sirius-env load --sh sirius > sirius-env/sirius.load`
+source sirius-env/sirius.load
+# also load cmake (system cmake is too old)
+eval "$(spack -e sirius-env load --sh cmake)"
+which cmake
+
+FC=mpif90 CXX=mpic++ CC=mpicc \
+          cmake \
+          -DCMAKE_INSTALL_PREFIX=./qebin \
+          -DQE_ENABLE_MPI=On \
+          -DQE_ENABLE_HDF5=On \
+          -DQE_ENABLE_SIRIUS=On \
+          -B $QESIRIUS_DIR/build \
+          -S $QESIRIUS_DIR
+
+cmake --build $QESIRIUS_DIR/build -j 8
+cmake --install $QESIRIUS_DIR/build
